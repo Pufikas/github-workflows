82ef1e3d7cd7ef63c90c3c01213707c8c521eebf
Pufikas <85221441+Pufikas@users.noreply.github.com>
2023-06-18 16:15:33 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,2 @@
-// https://www.npmjs.com/package/@actions/core
-// https://github.com/actions/setup-node
 
-// const core = require('@actions/core')
-// const github = require('@actions/github')
-
-// read https://dev.to/dancrtis/self-updating-github-profile-readme-with-javascript-lhm when i have a site built
-
-const fs = require("fs")
 
